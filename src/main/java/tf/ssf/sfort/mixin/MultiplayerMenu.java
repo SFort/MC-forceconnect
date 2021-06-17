23c2c05b8f6f701ab6630142725f7178e2c1a55e
SFort <github@ssf.tf>
2021-06-17 04:24:08 +0200

    1.17

--- src/main/java/tf/ssf/sfort/mixin/MultiplayerMenu.java
+++ src/main/java/tf/ssf/sfort/mixin/MultiplayerMenu.java
@@ -1,19 +1,19 @@
 package tf.ssf.sfort.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.ConnectScreen;
-import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.network.ServerInfo;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MultiplayerScreen.class)
-public class MultiplayerMenu {
+public abstract class MultiplayerMenu {
+	@Shadow protected abstract void connect(ServerInfo entry);
+
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		MinecraftClient.getInstance().openScreen(new ConnectScreen(new TitleScreen(), MinecraftClient.getInstance(), new ServerInfo("sforts hardcore","ssf.tf",false)));
+		this.connect(new ServerInfo("sforts hardcore","ssf.tf",false));
 	}
 }
